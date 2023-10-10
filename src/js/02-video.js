c7814bb72d351717b91da395cbae9e8e3a2047cd
IvaZach <ivazacherniuk@gmail.com>
2023-10-10 11:37:44 +0300

    second homework 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {});
+
+const onPlay = function (data) {
+  const timeInSeconds = data.seconds;
+  console.log(timeInSeconds);
+  localStorage.setItem('videoplayer-current-time', timeInSeconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+console.log(currentTime);
+
+player
+  .setCurrentTime(currentTime)
+  .then(function () {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
+
+player.off('play', onPlay);
